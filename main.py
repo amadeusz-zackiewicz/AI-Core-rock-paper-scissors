17e2420d3845890cb4e7d63e5b271c4fbcc1e46c
amadeusz-zackiewicz <amadeusz.zackiewicz@gmail.com>
2022-06-20 07:10:54 +0100

    Update main to run both versions of the game

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+from app.camera_rps import CameraRPS
 from app.manual_rps import ManualRPS
 
 if __name__ == "__main__":
-    g = ManualRPS()
+    print("\nPlease pick which version of the you would like to play:")
+    print("Input 'm' for manual version")
+    print("Input 'c' for camera version")
+    inp = input("[m / c]: ")
+    if inp == "m":
+        g = ManualRPS()
+    elif inp == "c":
+        g = CameraRPS()
+    else:
+        quit()
+
     while True:
-        g.play()
+        if not g.play():
+            g.quit()
+            break
+
+    
